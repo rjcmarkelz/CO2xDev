87b06c50642397adec43ed9c7ebcc69b7bba23d3
Robert Markelz <Cody@roberts-imac.gateway.2wire.net>
2013-10-08 21:59:38 -0700

    updated new values and files based on looking at both co2 and co2xdev genes

--- venn_diagrams.R
+++ venn_diagrams.R
@@ -2,12 +2,12 @@ library(VennDiagram)
 install.packages(pkgs="Vennerable",repos="http://r-forge.r-project.org/")
 install.packages(c("graph", "RBGL"), dependencies=TRUE)
 library(Vennerable)
-#setwd("/Users/Cody/Documents/Experiments/CO2xLeaf/Paper/New Phyt Submission/CO2xDevgitrepo")
+setwd("/Users/Cody/Documents/Experiments/CO2xLeaf/Paper/New Phyt Submission/CO2xDevgitrepo")
 setwd("/Users/Cody_2/git.repos/CO2xDev/")
 
-primordia <- read.table("mapman_co2_primordia_diff_only.txt", header = TRUE)
-expanding <- read.table("mapman_co2_expanding_diff_only.txt", header = TRUE)
-mature <- read.table("mapman_co2_mature_diff_only.txt", header = TRUE)
+primordia <- read.table("primordia_MAPMAN_cleaned.txt", header = TRUE)
+expanding <- read.table("expanding_MAPMAN_cleaned.txt", header = TRUE)
+mature <- read.table("fullexpanded_MAPMAN_cleaned.txt", header = TRUE)
 
 head(primordia)
 length(primordia)
@@ -20,15 +20,15 @@ mature$Probe_Set_ID <- as.character(mature$Probe_Set_ID)
 head(expanding)
 head(mature)
 
-length(primordia$Probe_Set_ID)#162
-length(expanding$Probe_Set_ID)#854
-length(mature$Probe_Set_ID)#1384
+length(primordia$Probe_Set_ID)#356
+length(expanding$Probe_Set_ID)#1395
+length(mature$Probe_Set_ID)#1908
 
 ########################
 ########################
 pri_exp <- intersect(primordia$Probe_Set_ID, expanding$Probe_Set_ID)
 pri_exp_length <- length(pri_exp)
-pri_exp_length #40
+pri_exp_length #99
 is.vector(pri_exp)
 pri_exp <- data.frame(pri_exp)
 pri_exp
@@ -48,7 +48,7 @@ write.table(pri_exp,file="pri_exp_data.txt",sep="\t", quote = FALSE,
 
 exp_mature <- intersect(expanding$Probe_Set_ID, mature$Probe_Set_ID)
 exp_mature_length <- length(exp_mature)
-exp_mature_length #434
+exp_mature_length #541
 
 exp_mature <- data.frame(exp_mature)
 exp_mature$value <- 1
@@ -65,7 +65,7 @@ write.table(exp_mature,file="exp_mature_data.txt",sep="\t", quote = FALSE,
 
 pri_mature <- intersect(mature$Probe_Set_ID, primordia$Probe_Set_ID)
 pri_mature_length <- length(pri_mature)
-pri_mature_length #75
+pri_mature_length #120
 
 pri_mature <- data.frame(pri_mature)
 pri_mature$value <- 1
@@ -82,7 +82,7 @@ write.table(pri_mature,file="pri_mature_data.txt",sep="\t", quote = FALSE,
 pri_exp_mat <- intersect(intersect(mature$Probe_Set_ID, primordia$Probe_Set_ID), 
 	                                expanding$Probe_Set_ID)
 pri_exp_mat_length <- length(pri_exp_mat)
-pri_exp_mat_length #21
+pri_exp_mat_length #34
 
 pri_exp_mat <- data.frame(pri_exp_mat)
 pri_exp_mat$value <- 1
@@ -98,33 +98,17 @@ write.table(pri_exp_mat,file="pri_exp_mat_data.txt",sep="\t", quote = FALSE,
 ########################
 ########################
 
-#########
-#Now not necessary, completed above 
-#Merge and output for MAPMAN
-#########
-# cat(pri_exp,file="outfiletest.txt",sep="\n")
-# merge1 <- merge(pri_exp, exp_mature, all = TRUE)
-# merge1
-
-# merge2 <- merge(merge1, pri_mature, all = TRUE)
-# merge2
-
-# merge3 <- merge(merge2, pri_exp_mat, all = TRUE)
-# merge3
-
-# write.table(merge3,file="venndiagram_data.txt",sep="\t")
-
 
 #VennDiagram Package
 ##################
 grid.newpage()
-venn.plot <- draw.triple.venn(area1    = 162,
-                              area2    = 854,
-                              area3    = 1384,
-                              n12      = 40,
-                              n23      = 434,
-                              n13      = 75,
-                              n123     = 21,
+venn.plot <- draw.triple.venn(area1    = 356,
+                              area2    = 1359,
+                              area3    = 1908,
+                              n12      = 99,
+                              n23      = 541,
+                              n13      = 120,
+                              n123     = 34,
                               euler.d  = TRUE,
                               scaled   = TRUE,
                               category = c("Primordia", "Expanding", "Mature"),
@@ -146,7 +130,7 @@ grid.draw(venn.plot)
 
 
 #test
-venn.plot2 <- Venn(SetNames= c("Primordia","          Expanding","Mature"), Weight=c(0,68,401,19,896,54,413,21))
+venn.plot2 <- Venn(SetNames= c("Primordia","Expanding","Mature"), Weight=c(0,68,401,19,896,54,413,21))
 plot(venn.plot2)
 
 
